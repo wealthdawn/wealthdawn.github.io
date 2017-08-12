67e81a7e0a42073387e871678e56b4b854c6aee7
zheng <wealthzheng@126.com>
2017-08-12 12:02:02 +0800

    添加一个二维码“ git commit -m 添加一个二维码“

--- js/showcode.js
+++ js/showcode.js
@@ -2,4 +2,8 @@ $(function(){
 	$("div.team-member img#one").click(function(){
 		$("div.code1").toggle(1500);
 	})
+	
+	$("div.team-member img#two").click(function(){
+		$("div.code2").toggle(1500);
+	})
 })
